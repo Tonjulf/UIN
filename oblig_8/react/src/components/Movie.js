4b231aaf52fc13f60bb802dc696b3642f9d95246
Tonjulf <97602424+Tonjulf@users.noreply.github.com>
2022-03-25 12:30:08 +0100

    Delete oblig_8 directory

--- oblig_8/react/src/components/Movie.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-named-as-default-member */
-import React from 'react'
-// eslint-disable-next-line import/no-named-as-default
-import Movies from './Movies'
-
-export default function Movie({ actor, title }) {
-  return <Movies title={title} actor={actor} />
-}
--- oblig_8/react/src/pages/Actor.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Actor() {
-  return <h1>Actor</h1>
-}
